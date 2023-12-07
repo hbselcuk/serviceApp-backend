d471acf4bc598e4ba2129bb86c91eddac30dad79
mightymarabu <sebastian.n.schmidt@web.de>
2023-12-07 11:49:30 +0100

    it's getting better

--- src/router/demo_router.py
+++ src/router/demo_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Json
 
 from db_conn import *
 
@@ -28,7 +28,8 @@ class getStuffData(BaseModel):
 class doStuffData(BaseModel):
     param_0: int
     param_1: int
-    param_2: str
+    param_2: int
+    brian: str
 
 # ids = json.dumps({"0":"getstuff","1":"10","2":"0"})
 
@@ -36,14 +37,13 @@ class doStuffData(BaseModel):
 async def getData(data: getStuffData):
     print (data.param_0, str(data.param_1), str(data.param_2))
     pgData = getPGData(data.param_0, str(data.param_1), str(data.param_2))
-    #pgData = getPGData((str(data.param_1), [str(data.param_2)]))
-    #pgData = getPGData("getstuff", 10, 0)
     return json.dumps(pgData)
 @router.post("/doStuff/")
 async def getData(data: doStuffData):
-    print ([data.param_0], [str(data.param_1)], [str(data.param_2)])
-    pgData = getPGData(data.param_0, str(data.param_1), str(data.param_2))
+    print (data.param_0, str(data.param_1), str(data.param_2), json.dumps(data.brian))
+    pgData = getPGData(data.param_0, str(data.param_1), str(data.brian))
     #pgData = getPGData((str(data.param_1), [str(data.param_2)]))
     #pgData = getPGData("getstuff", 10, 0)
     return json.dumps(pgData)
+    #return JSONResponse(content=json.dumps(pgData), media_type="application/json")
 
